90dc927327c86ac68de901797cf424b5cf579c48
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-10-30 22:13:13 +0600

    initial layout with scroll snap functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import "./app.scss";
+
 function App() {
   return (
-    <>
-      <div>Hello</div>
-    </>
+    <div>
+      <section>Parallax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section>Contact</section>
+    </div>
   );
 }
 
