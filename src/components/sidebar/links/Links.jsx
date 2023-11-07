cb8b6871a967c5822104ca8c69f610df919d1148
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-11-07 23:58:01 +0600

    Sidebar menu link animations

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,14 +1,42 @@
+import { motion } from "framer-motion";
+
+const variants = {
+  open: {
+    transition: { staggerChildren: 0.1, delayChildren: 0.4 },
+  },
+  closed: {
+    transition: { staggerChildren: 0.05, staggerDirection: -1 },
+  },
+};
+
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+  },
+};
+
 const Links = () => {
   const items = ["Homepage", "Services", "Portfolio", "Contact", "About"];
 
   return (
-    <ul className="links">
+    <motion.ul className="links" variants={variants}>
       {items.map((item) => (
-        <a href={`#${item}`} key={item}>
+        <motion.a
+          key={item}
+          href={`#${item}`}
+          variants={itemVariants}
+          whileHover={{ scale: 1.07 }}
+          whileTap={{ scale: 0.97 }}
+        >
           {item}
-        </a>
+        </motion.a>
       ))}
-    </ul>
+    </motion.ul>
   );
 };
 
