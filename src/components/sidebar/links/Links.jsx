44fdbbb203965224c3639f06f8da35fc95347bec
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-11-07 23:22:15 +0600

    Rendered sidebar links

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,5 +1,15 @@
 const Links = () => {
-  return <div>Links</div>;
+  const items = ["Homepage", "Services", "Portfolio", "Contact", "About"];
+
+  return (
+    <ul className="links">
+      {items.map((item) => (
+        <a href={`#${item}`} key={item}>
+          {item}
+        </a>
+      ))}
+    </ul>
+  );
 };
 
 export default Links;
