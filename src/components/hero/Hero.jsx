33f18891e8681cdd3d1cac5178735e4cbdc2f49c
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-11-08 17:53:31 +0600

    intro in hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,7 +3,21 @@ import "./hero.scss";
 const Hero = () => {
   return (
     <div className="hero">
-      <div className="imgContainer">
+      <div className="wrapper">
+        <div className="textContainer">
+          <h2>SAJID HOSSAIN</h2>
+          <h1>Front-end Web Developer</h1>
+
+          <div className="buttons">
+            <button>See latest works</button>
+            <button>Contact me</button>
+          </div>
+
+          <img src="./scroll.png" alt="scroll" />
+        </div>
+      </div>
+
+      <div className="imageContainer">
         <img src="./hero-img.png" alt="hero" />
       </div>
     </div>
