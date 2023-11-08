17d249c6a820bbec2181be386bf135ecab636421
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-11-08 16:29:31 +0600

    hero image setup

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,13 @@
+import "./hero.scss";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="imgContainer">
+        <img src="./hero-img.png" alt="hero" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
