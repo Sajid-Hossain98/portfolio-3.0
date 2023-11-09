e4529d0b10664867f8fb1f2dbbfcbdedf390c7ca
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-11-09 20:56:04 +0600

    animations for services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,35 +1,58 @@
 import "./services.scss";
 import { motion } from "framer-motion";
 
+const servicesVariants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
 const Services = () => {
   return (
     <div className="services-container">
-      <motion.div className="services" initial="initial" whileInView="animate">
-        <motion.div className="textContainer">
+      <motion.div
+        className="services"
+        variants={servicesVariants}
+        initial="initial"
+        whileInView="animate"
+      >
+        <motion.div className="textContainer" variants={servicesVariants}>
           <p>
             I focus on helping your brand grow
             <br /> and move forward
           </p>
           <hr />
         </motion.div>
-        <motion.div className="titleContainer">
+        <motion.div className="titleContainer" variants={servicesVariants}>
           <div className="title">
             <img src="./people.webp" alt="group of people" />
 
             <h1>
-              <b>Unique</b> Ideas
+              <motion.b whileHover={{ color: "orange" }}>Unique</motion.b> Ideas
             </h1>
           </div>
 
           <div className="title">
             <h1>
-              <b>For Your</b> Ideas
+              <motion.b whileHover={{ color: "orange" }}>For Your</motion.b>{" "}
+              Ideas
             </h1>
 
             <button>WHAT WE DO?</button>
           </div>
         </motion.div>
-        <motion.div className="listContainer">
+        <motion.div className="listContainer" variants={servicesVariants}>
           <motion.div
             className="box"
             whileHover={{ backgroundColor: "lightgray", color: "black" }}
